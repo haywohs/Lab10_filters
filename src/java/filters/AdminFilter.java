49112ae3ef17d0e21c139cee23e644dbfe77b89c
Haywo <ru8391@gmail.com>
2022-07-17 12:11:22 -0600

    a new Project for Lab10_filters

--- src/java/filters/AdminFilter.java
+++ src/java/filters/AdminFilter.java
@@ -11,37 +11,34 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-public class AuthenticationFilter implements Filter {
+public class AdminFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        // before we can use HttpServletRequest or HttpServletResponse methods
-        // we must cast the ServletRequest and ServletResponse objects as the correct type
         HttpServletRequest httpRequest = (HttpServletRequest) request;
         HttpSession session = httpRequest.getSession();
         String email = (String)session.getAttribute("email");
-        
-        if( email == null ){
+        String admin = "cprg352+admin@gmail.com";
+
+        if(!email.equals(admin)){
             HttpServletResponse httpResponse = (HttpServletResponse) response;
-            httpResponse.sendRedirect("login");
+            httpResponse.sendRedirect("notes");
             return;
         }
         
-        // any code before chain.doFilter
-        // will be executed before the servlet
         chain.doFilter(request, response);
-        // any code after chain.doFilter
-        // will be executed after the servlet
-        
     }
 
-    // the init and destroy methods are not needed in this case
+    
+    
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
+        
     }
 
     @Override
     public void destroy() {
+        
     }
 
 }
